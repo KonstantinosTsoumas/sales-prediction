270c5f30cbb240f6073fe36f85eeb696bc7fffcb
Konstantinos <tsoumascon@gmail.com>
2023-10-02 20:04:58 +0200

    modify relative paths due to accessibility errors

--- config.py
+++ config.py
@@ -1,22 +1,24 @@
-# config.py
+import os
 
 # Directories
-ARTIFACTS_DIR = 'artifacts'
-SRC_DIR = 'src'
-COMPONENTS_DIR = f'{SRC_DIR}/components'
-PIPELINE_DIR = f'{SRC_DIR}/pipeline'
-LOGS_DIR = 'logs'
-INPUT_DIR = 'input'
-TESTS_DIR = 'tests'
+ARTIFACTS_DIR = "../artifacts"
+SRC_DIR = '../src'
+COMPONENTS_DIR = '../src/components'
+PIPELINE_DIR = '../src/pipeline'
+LOGS_DIR = '../logs'
+INPUT_DIR = '../input'
+TESTS_DIR = '../tests'
 
 # Artifacts Paths
-ENCODED_DATA_CSV_PATH = f'{ARTIFACTS_DIR}/encoded_data.csv'
-MODEL_PKL_PATH = f'{ARTIFACTS_DIR}/model.pkl'
-PREPROCESSOR_PKL_PATH = f'{ARTIFACTS_DIR}/preprocessor.pkl'
-ENCODER_OBJ_FILE_PATH = f'{ARTIFACTS_DIR}/encoder.pkl'
+ENCODED_DATA_CSV_PATH = '../artifacts/encoded_data.csv'
+MODEL_PKL_PATH = '../artifacts/model.pkl'
+PREPROCESSOR_PKL_PATH = '../artifacts/preprocessor.pkl'
+ENCODER_OBJ_FILE_PATH = '../artifacts/encoder.pkl'
 
 # Data Paths
-INPUT_DATA_CSV = f'{INPUT_DIR}/DataCoSupplyChainDataset.csv'
+TRAIN_DATA_PATH = "../artifacts"
+TEST_DATA_PATH = "../artifacts"
+RAW_DATA_PATH = '../artifacts/DataCoSupplyChainDataset.csv'
 
 # Log Paths
 LOG_FILE_PATH = f'{LOGS_DIR}/app.log'

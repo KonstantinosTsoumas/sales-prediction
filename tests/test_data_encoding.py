dec31091b012acfbb710ffe742afc2066b12c990
Konstantinos <tsoumascon@gmail.com>
2023-10-02 20:08:31 +0200

    add tests for data encoding regarding its enconding

---
+++ tests/test_data_encoding.py
@@ -0,0 +1,34 @@
+import unittest
+import pandas as pd
+import os
+from src.components.data_encoding import DataEncoding, DataEncodingConfig
+
+class TestDataEncoding(unittest.TestCase):
+
+    def setUp(self):
+        self.data_encoding = DataEncoding()
+
+        self.test_data = pd.DataFrame({
+            'A': ['apple', 'banana', 'apple', 'apple', 'banana'],
+            'B': ['dog', 'cat', 'dog', 'dog', 'bird'],
+            'C': [1, 2, 3, 1, 2]
+        })
+
+    def test_handle_categorical_encoding(self):
+        encoded_df = self.data_encoding.handle_categorical_encoding(self.test_data)
+
+        self.assertTrue('A' not in encoded_df.columns)
+        self.assertTrue('A_apple' not in encoded_df.columns)
+        self.assertTrue('B' not in encoded_df.columns)
+
+        self.assertTrue('A_banana' in encoded_df.columns)
+        self.assertTrue('B_dog' in encoded_df.columns)
+        self.assertTrue('B_cat' in encoded_df.columns)
+
+        # Clean up the generated CSV file§
+        if os.path.exists(DataEncodingConfig.encoded_data_csv_path):
+            os.remove(DataEncodingConfig.encoded_data_csv_path)
+
+
+if __name__ == '__main__':
+    unittest.main()

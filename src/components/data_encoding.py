3fef414344ba61ac4e299db500dde56ff69aaea7
Konstantinos <tsoumascon@gmail.com>
2023-10-05 10:29:44 +0200

    adjust code formatting

--- src/components/data_encoding.py
+++ src/components/data_encoding.py
@@ -7,11 +7,14 @@ import pandas as pd
 
 from src.exception import CustomException
 from src.logger import logging
+from src.utils import save_object
 from config import ARTIFACTS_DIR
+
 
 @dataclass
 class DataEncodingConfig:
     artifacts_dir: str = ARTIFACTS_DIR
+
 
 class DataEncoding:
     def __init__(self, unique_value_threshold: Optional[int] = 20):
@@ -32,21 +35,27 @@ class DataEncoding:
         df: A df with encoded values
         """
         try:
-            logging.info(f"Starting the encoding columns functionality. Initial DataFrame shape: {df.shape}")
+            logging.info(
+                f"Starting the encoding columns functionality. Initial DataFrame shape: {df.shape}"
+            )
             # Initialize label encoder and its lists
             label_encoder = LabelEncoder()
             label_encode_cols = []
             one_hot_encode_cols = []
 
             # Distinguish which columns needs label or one-hot encoding based on pre (or user) defined threshold.
-            for col in df.select_dtypes(include=['object']).columns:
+            for col in df.select_dtypes(include=["object"]).columns:
                 if df[col].nunique() > self.unique_value_threshold:
                     label_encode_cols.append(col)
                 else:
                     one_hot_encode_cols.append(col)
 
-            logging.info(f"Starting encoding columns. Columns to label encode: {label_encode_cols}")
-            logging.info(f"Starting encoding columns. Columns to one-hot encode: {one_hot_encode_cols}")
+            logging.info(
+                f"Starting encoding columns. Columns to label encode: {label_encode_cols}"
+            )
+            logging.info(
+                f"Starting encoding columns. Columns to one-hot encode: {one_hot_encode_cols}"
+            )
 
             # Perform label encoding on categorical columns with a variety of different values.
             for col in label_encode_cols:
@@ -54,12 +63,16 @@ class DataEncoding:
 
             # Perform one-hot encoding on categorical columns with a small number of unique values.
             df = pd.get_dummies(df, columns=one_hot_encode_cols, drop_first=True)
-            logging.info('Encoding completed successfully.')
+            logging.info("Encoding completed successfully.")
 
-            logging.info('Starting saving the dataset as a csv file in the "artifacts" directory.')
-            output_path = os.path.join(self.data_encoding_config.artifacts_dir, 'encoded_data.csv')
+            logging.info(
+                'Starting saving the dataset as a csv file in the "artifacts" directory.'
+            )
+            output_path = os.path.join(
+                self.data_encoding_config.artifacts_dir, "encoded_data.csv"
+            )
             df.to_csv(output_path, index=False)
-            logging.info('Saving the dataset has been successfully completed.')
+            logging.info("Saving the dataset has been successfully completed.")
 
             return df
 

eb0637d70dbbfde4f826f1017bf736b1dcc4acec
Konstantinos <tsoumascon@gmail.com>
2023-10-10 11:18:34 +0200

    modify paths and remove unused ones

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 # Directories
-ARTIFACTS_DIR = "../artifacts"
+ARTIFACTS_DIR = "../supply-chain-prediction/artifacts/"
 SRC_DIR = '../src'
 COMPONENTS_DIR = '../src/components'
 PIPELINE_DIR = '../src/pipeline'
@@ -12,14 +12,14 @@ TESTS_DIR = '../tests'
 # Artifacts Paths
 ENCODED_DATA_CSV_PATH = '../artifacts/encoded_data.csv'
 MODEL_PKL_PATH = '../artifacts/model.pkl'
-PREPROCESSOR_PKL_PATH = '../artifacts/preprocessor.pkl'
+PREPROCESSOR_PKL_PATH = '../supply-chain-prediction/artifacts/preprocessor.pkl'
 ENCODER_OBJ_FILE_PATH = '../artifacts/encoder.pkl'
 
 # Data Paths
 TRAIN_DATA_PATH = "../artifacts"
 TEST_DATA_PATH = "../artifacts"
-RAW_DATA_PATH = '../artifacts/DataCoSupplyChainDataset.csv'
-TRANSFORMED_DATA_PATH = '../artifacts/transformed_data.csv'
+RAW_DATA_PATH = 'input/DataCoSupplyChainDataset.csv'
+TRANSFORMED_DATA_PATH = '../supply-chain-prediction/artifacts/'
 
 # Log Paths
 LOG_FILE_PATH = f'{LOGS_DIR}/app.log'

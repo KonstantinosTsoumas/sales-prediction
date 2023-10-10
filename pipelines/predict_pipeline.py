df77bfd0dadaba510c1b5c5cb9b354455a9673c8
Konstantinos <tsoumascon@gmail.com>
2023-10-10 17:24:12 +0200

    create custom data class

---
+++ pipelines/predict_pipeline.py
@@ -0,0 +1,31 @@
+
+
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+class CustomData:
+    def __init__(self,
+    Order_Item_Product_Price: float,
+    Order_Item_Total: float,
+    Order_Item_Quantity: float,
+    Product_Name: str,
+    Product_Price: float,
+    Category_Name: str,
+    Order_Item_Discount: float):
+
+        self.Order_Item_Product_Price = order_item_price
+
+        self.Order_Item_Total = order_total
+
+        self.Order_Item_Quantity = order_item_quantity
+
+        self.Product_Name = product_name
+
+        self.Product_Price = product_price
+
+        self.Category_Name = category_name
+
+        self.Order_Item_Discount = order_item_discount
+

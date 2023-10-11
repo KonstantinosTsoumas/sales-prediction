cc5b438f8ab03796e7b0e77c9de9f5908c69a633
Konstantinos <tsoumascon@gmail.com>
2023-10-11 17:36:05 +0200

    Collect and format input data from form for prediction

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask, request, render_template
 import pandas as pd
 import numpy as np
 
-application = Flask(__name__)
+import pipelines.predict_pipeline
+from pipelines.predict_pipeline import CustomData, PredictPipeline
 
-app = application
+app = Flask(__name__)
+
+app.debug = True
 
 @app.route('/')
 def index():
@@ -15,3 +18,15 @@ def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
     else:
+        input_data=pipelines.predict_pipeline.CustomData(
+        Order_Item_Product_Price= float(request.form.get('Order_Item_Product_Price')),
+        Order_Item_Total= float(request.form.get('Order_Item_Total')),
+        Order_Item_Quantity= float(request.form.get('Order_Item_Quantity')),
+        Product_Price = float(request.form.get('Product_Price')),
+        Order_Item_Discount = float(request.form.get('Order_Item_Discount')),
+        Category_Name = request.form.get('Category_Name'),
+        Product_Name= request.form.get('Product_Name')
+        )
+
+
+
